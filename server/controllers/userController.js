f3bad139fa0f21ae5ce4e80b43f5ae96711a8529
sanjay kumar <sanjay.prajapati80@gmail.com>
2022-03-07 11:14:00 +0530

    login route completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
+const sendToken = require("../utils/jwtToken");
 
+// User Registration
 exports.userRegister = async (req, res, next) => {
   try {
     const { name, email, password, mobile } = req.body;
@@ -9,7 +11,39 @@ exports.userRegister = async (req, res, next) => {
       mobile,
       password,
     });
-    res.status(201).json({ success: true, user });
+
+    sendToken(user, 200, res);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+// User Login
+exports.userLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res
+        .status(400)
+        .json({ success: false, message: "Please enter Email & Password" });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    sendToken(user, 200, res);
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const { userRegister, fileuplad } = require("../controllers/userController");
+const {
+  userRegister,
+  fileuplad,
+  userLogin,
+} = require("../controllers/userController");
 const { upload } = require("../middlewares/imageUpload");
 
 router.route("/register").post(userRegister);
+router.route("/login").post(userLogin);
 router.route("/fileupload").post(upload.single("file"), fileuplad);
 
 module.exports = router;
