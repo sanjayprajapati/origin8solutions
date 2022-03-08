6103da7221db8905c7b2669f3668f705a22b4d33
sanjay kumar <sanjay.prajapati80@gmail.com>
2022-03-08 09:36:40 +0530

    switch creatoon done

--- server/controllers/switchController.js
+++ server/controllers/switchController.js
@@ -3,11 +3,25 @@ const Appartment = require("../models/Appartment");
 const Switeches = require("../models/Swithes");
 
 exports.createSwitch = catchAsyncErrors(async (req, res, next) => {
+  const userId = req.user.id;
+
   const apprtId = req.user.appartmentId.toString();
 
   const appartment = await Appartment.findById(apprtId);
-  console.log(appartment.rooms[req.params.id]);
 
+  const roomName = appartment.rooms[req.params.id];
+
+  const { switchName } = req.body;
+  if (roomName == "") {
+    const switches = await Switeches.create({
+      switchName,
+      userId,
+      roomName,
+    });
+    res.status(200).json({ success: true, switches });
+  }
+  res
+    .status(200)
+    .json({ success: false, message: "There is no rooms are available" });
   //const swithes = await Switeches.create(req.body);
-  res.status(200).json({ success: true, message: "Ok" });
 });
